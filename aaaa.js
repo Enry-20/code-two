ef24a5fdd5924a48572136fc2147bed09ddabc91
Enry-20 <enriquecuellar7u7@gmail.com>
2020-10-19 03:43:40 +0100

    second-commit

---
+++ aaaa.js
@@ -0,0 +1,17 @@
+//let myName = document.getElementsByClassName('next');
+// myName.onclick= function(){
+//     let thsrc= myName.getAttribute('src');
+//     if ( thsrc === "img/niño-skate.jpeg"){
+//         myName.setAttribute('src', "img/lifeonvenus.jpeg")
+//     } else {myName.setAttribute('src',"img/niño-skate.jpeg");}
+// }
+
+
+
+
+
+
+
+
+
+
